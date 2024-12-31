9e8cc61e79978c524fb4b63a54ac7d0dcf3e2b79
Nimi <takinroy@ualberta.ca>
2024-12-31 16:51:26 -0700

    Cleaned up the create flashcards page

--- app/create-flashcards/page.tsx
+++ app/create-flashcards/page.tsx
@@ -2,9 +2,10 @@
 
 import { useUser } from "@clerk/nextjs";
 import FlashcardForm from "@/components/FlashcardForm";
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { db } from "@/firebase";
 import { writeBatch, doc, collection, getDoc } from "firebase/firestore";
+import { useRouter } from "next/navigation";
 
 type FlashCardFormProps = {
   id: number;
@@ -16,7 +17,7 @@ type FlashCardFormProps = {
 type FlashcardSaveProps = {
   back: string;
   front: string;
-}
+};
 
 export default function CreateFlashcards() {
   const [flashcards, setFlashcards] = useState<FlashCardFormProps[]>(
@@ -28,12 +29,21 @@ export default function CreateFlashcards() {
   );
   const [name, setName] = useState("");
   const { isLoaded, isSignedIn, user } = useUser();
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const router = useRouter();
+
+  const dummyRef = useRef<HTMLDivElement>(null);
 
   const addFlashcard = () => {
     setFlashcards([
       ...flashcards,
       { id: flashcards.length, question: "", answer: "" },
     ]);
+
+    setTimeout(() => {
+      dummyRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 100);
   };
 
   const updateFlashcard = (
@@ -60,6 +70,8 @@ export default function CreateFlashcards() {
       alert("You must be logged in to save flashcards");
       return;
     }
+    setIsLoading(true);
+    setProgress(0);
 
     const batch = writeBatch(db);
     const userDocRef = doc(collection(db, "users"), user.id);
@@ -83,62 +95,81 @@ export default function CreateFlashcards() {
 
       const flashcardRef = collection(userDocRef, name);
 
+      let savedCount = 0;
       flashcards.forEach((flashcard) => {
         const cardDocRef = doc(flashcardRef);
 
         const transformedFlashcard: FlashcardSaveProps = {
           front: flashcard.question,
-          back: flashcard.answer
-        }
+          back: flashcard.answer,
+        };
 
         batch.set(cardDocRef, transformedFlashcard);
+
+        savedCount++;
+        setProgress(Math.round((savedCount / flashcards.length) * 100));
       });
 
       await batch.commit();
+      setProgress(100);
+      setTimeout(() => router.push("/collections"), 500);
     } catch (error) {
       console.error("Error saving flashcards:", error);
       alert(
         "An error occured while saving the flashcard set. Please try again."
       );
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
-    <div className="flex flex-col justify-center mt-10 items-center">
+    <div className="relative flex flex-col justify-center mt-10 items-center">
+      {isLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+          <div className="flex flex-col text-center items-center">
+            <div className="loader border-t-4 border-white rounded-full w-16 h-16 animate-spin"></div>
+            <p className="mt-3 text-white text-base 2xl:text-lg antialiased">
+              Saving... {progress}%
+            </p>
+          </div>
+        </div>
+      )}
+
       <h3 className="mb-6 scroll-m-20 antialiased text-4xl font-bold tracking-tight lg:text-5xl">
         Create Flashcards
       </h3>
-      <h2 className="scroll-m-20 antialiased text-xl font-semibold tracking-tight lg:text-2xl">
-        Title
-      </h2>
       <input
         className="my-5 flex-1 peer w-5/12 bg-transparent border-b-2 placeholder-zinc-500 dark:placeholder-slate-300 border-zinc-500 dark:border-slate-300  dark:text-white focus:outline-none focus:ring-0 focus:border-zinc-900 dark:focus:border-white antialiased"
         value={name}
         placeholder="Enter a name for your flashcard set"
         onChange={(e) => setName(e.target.value)}
       />
-      {flashcards.map((card) => (
+      {flashcards.map((card, index) => (
         <FlashcardForm
           key={card.id}
-          id={card.id}
+          id={index}
           question={card.question}
           answer={card.answer}
           onUpdate={updateFlashcard}
           onDelete={deleteFlashcard}
         />
       ))}
-      <button
-        onClick={addFlashcard}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Add Flashcard
-      </button>
-      <button
-        onClick={saveFlashcards}
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Save Flashcard
-      </button>
+      <div ref={dummyRef} />
+      <div className="flex flex-row gap-4 mt-5">
+        <button
+          onClick={addFlashcard}
+          className="px-4 py-2 dark:bg-slate-100 dark:text-black bg-black text-white rounded-md hover:scale-105 transition-transform duration-300"
+        >
+          Add Flashcard
+        </button>
+        <button
+          onClick={saveFlashcards}
+          className="px-4 py-2 dark:bg-slate-100 dark:text-black bg-black text-white rounded-md hover:scale-105 transition-transform duration-300"
+        >
+          Save Flashcard
+        </button>
+      </div>
     </div>
   );
 }
--- components/FlashcardForm.tsx
+++ components/FlashcardForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, TextField } from "@mui/material";
+import { Box } from "@mui/material";
 import { Trash2 } from "lucide-react";
 
 type FlashcardFormProps = {
@@ -18,9 +18,15 @@ const FlashcardForm: React.FC<FlashcardFormProps> = ({
   onUpdate,
   onDelete,
 }) => {
+  // Function to handle auto-resizing
+  const handleResize = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    event.target.style.height = "auto"; // Reset height to auto to allow shrinking
+    event.target.style.height = `${event.target.scrollHeight}px`; // Set to scroll height
+  };
+
   return (
-    <div className="w-5/12 flex flex-col mb-4 dark:bg-sky-700 bg-blue-100 rounded-alg">
-      <div className="p-4 border-b border-zinc-400">
+    <div className="2xl:w-5/12 xl:w-7/12 flex flex-col mb-4 dark:bg-sky-600 bg-blue-100 rounded-alg">
+      <div className="p-4 border-b border-zinc-400 dark:border-slate-300">
         <div className="flex flex-row">
           <p className="flex-1 antialiased">{id + 1}</p>
           <Trash2
@@ -31,18 +37,25 @@ const FlashcardForm: React.FC<FlashcardFormProps> = ({
       </div>
       <div className="p-6">
         <div className="flex flex-row gap-6">
-          <input
-            className="flex-1 peer w-full bg-transparent border-b-2 placeholder-zinc-500 dark:placeholder-slate-300 border-zinc-500 dark:border-slate-300  dark:text-white focus:outline-none focus:ring-0 focus:border-zinc-900 dark:focus:border-white antialiased"
+          {/* Question Textarea */}
+          <textarea
+            className="flex-1 peer w-full bg-transparent border-b-2 placeholder-zinc-500 dark:placeholder-slate-300 border-zinc-500 dark:border-slate-300 dark:text-white focus:outline-none focus:ring-0 focus:border-zinc-900 dark:focus:border-white antialiased resize-none"
             value={question}
             placeholder="Question"
+            onInput={handleResize}
             onChange={(e) => onUpdate(id, "question", e.target.value)}
-          />
-          <input
-            className="flex-1 peer w-full bg-transparent border-b-2 placeholder-zinc-500 dark:placeholder-slate-300 border-zinc-500 dark:border-slate-300 dark:text-white focus:outline-none focus:ring-0 focus:border-zinc-900 dark:focus:border-white antialiased"
+            rows={1} // Minimum height
+          ></textarea>
+
+          {/* Answer Textarea */}
+          <textarea
+            className="flex-1 peer w-full bg-transparent border-b-2 placeholder-zinc-500 dark:placeholder-slate-300 border-zinc-500 dark:border-slate-300 dark:text-white focus:outline-none focus:ring-0 focus:border-zinc-900 dark:focus:border-white antialiased resize-none"
             value={answer}
             placeholder="Answer"
+            onInput={handleResize}
             onChange={(e) => onUpdate(id, "answer", e.target.value)}
-          />
+            rows={1} 
+          ></textarea>
         </div>
       </div>
     </div>
